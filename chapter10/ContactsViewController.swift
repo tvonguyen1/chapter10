b80e405d0e829b53bbde65e852ea7d9f8065da17
Nina Vo <nhattienvo21@gmail.com>
2022-04-04 17:07:32 -0500

    commit

--- chapter10/ContactsViewController.swift
+++ chapter10/ContactsViewController.swift
@@ -9,12 +9,52 @@ import UIKit
 
 class ContactsViewController: UIViewController {
 
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtCell: UITextField!
+    @IBOutlet weak var txtZip: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var lblBirthdate: UILabel!
+    @IBOutlet weak var btnChange: UIButton!
+    @IBOutlet weak var sgmtEditMode: UISegmentedControl!
+    
     @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
+        self.changeEditMode(self)
     }
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+        //dispose of any resourcs that can be recreated
+    }
+    
+    @IBAction func changeEditMode(_ sender: Any) {
+        let textFields: [UITextField] = [txtName, txtZip, txtCell, txtCity, txtAddress, txtState, txtEmail,txtPhone]
+        if sgmtEditMode.selectedSegmentIndex == 0{
+            for textField in textFields {
+                textField.isEnabled = false
+                textField.borderStyle = UITextField.BorderStyle.none
+                
+            }
+            btnChange.isHidden = true
+        }
+        else if sgmtEditMode.selectedSegmentIndex == 1 {
+            for textField in textFields {
+                textField.isEnabled = true
+                textField.borderStyle = UITextField.BorderStyle.roundedRect
+            }
+            btnChange.isHidden = false
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
             super.viewWillAppear(animated)
             //register that the keyboard has beed ndisplayed
